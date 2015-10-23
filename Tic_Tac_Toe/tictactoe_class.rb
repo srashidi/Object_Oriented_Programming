4fe7333f80e34827e51da619139c91b48a7f0aa8
Shaunty Rashidi <shauntyrashidi@Shaunty-Rashidis-Computer.local>
2015-10-22 20:04:46 -0700

    Alternating placement of X's and O's working

--- Tic_Tac_Toe/tictactoe_class.rb
+++ Tic_Tac_Toe/tictactoe_class.rb
@@ -1,19 +1,33 @@
 # The general class for the game
 class TicTacToe
 
-	attr_accessor :game_board, :row1, :row2, :row3
+	WIN = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
+
+	attr_accessor :row1, :row2, :row3
 
 	def initialize
-		@game_board = GameBoard.new
+		@row1 = "   |   |   "
+		@row2 = "   |   |   "
+		@row3 = "   |   |   "
 		puts "You have initiated a game of Tic-Tac-Toe!"
 		puts "When choosing a spot on the board, use the"
 		puts "numbers 1 through 9 like so:"
 		puts "\n"
 		puts " 1 | 2 | 3 \n-----------"
 		puts " 4 | 5 | 6 \n-----------"
-		puts " 7 | 8 | 9 \n"
+		puts " 7 | 8 | 9 \n\n"
 		puts "X goes first."
 		round("X")
+	end
+
+	def display
+		puts "\n"
+		puts row1
+		puts "-----------"
+		puts row2
+		puts "-----------"
+		puts row3
+		puts "\n"
 	end
 
 	def place_piece(position,piece)
@@ -36,36 +50,39 @@ class TicTacToe
 			spot = row3[5]
 		when 9
 			spot = row3[9]
+		when "help"
+			puts "\n"
+			puts " 1 | 2 | 3 \n-----------"
+			puts " 4 | 5 | 6 \n-----------"
+			puts " 7 | 8 | 9 \n\n"
+			round(piece)
 		else
-			puts "Not a valid entry. Try again."
+			puts "\nNot a valid entry. Try again."
 			round(piece)
 		end
 		if spot =~ /\s/
 			case position
 			when 1
-				@gameboard.row1[1] = piece
+				row1[1] = piece
 			when 2
-				@gameboard.row1[5] = piece
+				row1[5] = piece
 			when 3
-				@gameboard.row1[9] = piece
+				row1[9] = piece
 			when 4
-				@gameboard.row2[1] = piece
+				row2[1] = piece
 			when 5
-				@gameboard.row2[5] = piece
+				row2[5] = piece
 			when 6
-				@gameboard.row2[9] = piece
+				row2[9] = piece
 			when 7
-				@gameboard.row3[1] = piece
+				row3[1] = piece
 			when 8
-				@gameboard.row3[5] = piece
+				row3[5] = piece
 			when 9
-				@gameboard.row3[9] = piece
+				row3[9] = piece
 			end
-			puts "Your #{piece} piece was placed in position #{position}."
-			if win?
-				puts "#{piece} wins!"
-				# break
-			end
+			puts "\nYour #{piece} piece was placed in position #{position}."
+			display
 			if piece == "X"
 				piece = "O"
 			elsif piece == "O"
@@ -73,37 +90,16 @@ class TicTacToe
 			end
 			round(piece)		
 		else
-			puts "There is already a piece in position #{position}. Try again."
+			puts "\nThere is already a piece in position #{position}. Try again."
 			round(piece)
 		end
 	end
 
 	def round(piece)
-		puts "Choose position for #{piece}:"
-		position = gets.chomp.to_i
+		puts "\nChoose position for #{piece}, or ask for 'help':"
+		position = gets.chomp.strip.downcase
+		position = position.to_i unless position == "help"
 		place_piece(position,piece)
-	end
-
-	class GameBoard
-
-		attr_accessor :row1, :row2, :row3
-
-		def initialize
-			@row1 = "   |   |   "
-			@row2 = "   |   |   "
-			@row3 = "   |   |   "
-		end
-
-		def display
-			puts "\n"
-			puts row1
-			puts "-----------"
-			puts row2
-			puts "-----------"
-			puts row3
-			puts "\n"
-		end
-
 	end
 
 end
